9282d1498a1876390b55a1e642846ba365ace1f7
ChowonKim <97011721+chowon986@users.noreply.github.com>
2023-07-20 20:00:16 +0900

    초기 커밋.

---
+++ Source/PenguinNoWayHome/PenguinNoWayHomeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PenguinNoWayHomeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PENGUINNOWAYHOME_API APenguinNoWayHomeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
